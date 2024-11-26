8c1e6e1136c701f2f55962258c63a620439bcde2
Paul Semehen <psemehen@gmail.com>
2024-11-26 04:39:06 -0500

    Removes ActiveRecord dependencies

--- app/controllers/concerns/json_response.rb
+++ app/controllers/concerns/json_response.rb
@@ -1,11 +1,7 @@
-require "active_record"
-
 module JsonResponse
   extend ActiveSupport::Concern
 
   included do
-    rescue_from ActiveRecord::RecordNotFound, with: :not_found
-    rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity
     rescue_from ArgumentError, with: :handle_argument_error
     rescue_from ActiveModel::ValidationError, with: :handle_validation_error
   end
@@ -19,14 +15,6 @@ module JsonResponse
   end
 
   private
-
-  def not_found(exception)
-    json_error(exception.message, :not_found)
-  end
-
-  def unprocessable_entity(exception)
-    json_error(exception.record.errors.full_messages.join(", "), :unprocessable_entity)
-  end
 
   def handle_argument_error(exception)
     json_error("Invalid argument: #{exception.message}", :bad_request)
